3190aeac109f0484ffc2d574a83ef2f5c4f50d67
Harika Nagapuri <jobsforharika@gmail.com>
2025-06-23 08:54:24 +0530

    Day 10 files updated, react JS installed.

---
+++ day10-javascript/app.js
@@ -0,0 +1,6 @@
+console.log("Namaste, JavaScript!");
+console.log("Mana first JS program! Bagundhaa?");
+
+let isAdult = age >= 18;
+
+console.log("Adult aa?", isAdult);
---
+++ day10-javascript/homework-food-order.js
@@ -0,0 +1,16 @@
+Requirements:
+
+1. Variables create cheyali:
+
+   • Restaurant name (Venkatesh Bhattu, Dharani, local mess, etc.)
+   • Food item name (Chicken Biryani, Fish curry, Pulihora, etc.)
+   • Price
+   • Quantity
+   • Customer name
+   • Delivery kavala leda (true/false)
+   • Delivery area (MVP Colony, Dwaraka Nagar, Beach Road, etc.)
+
+2. Total price calculate cheyali (price × quantity)
+3. Delivery charges add cheyali if needed (₹40 delivery ki)
+4. Order summary console.log tho show cheyali
+5. Proper scopes use cheyali (different parts ki functions create cheyali)
